3b54660087b10389e9b232f7ac307ff52722b1c0
bloblo1001 <154530339+bloblo1001@users.noreply.github.com>
2023-12-21 15:05:07 +0100

    Update pokedex.ts

--- data/pokedex.ts
+++ data/pokedex.ts
@@ -19734,4 +19734,17 @@ export const Pokedex: {[speciesid: string]: SpeciesData} = {
 		eggGroups: ["Undiscovered"],
 		gen: 5,
 	},
+	myownmon: {
+		num: 1,
+		name: "myownmon",
+		types: ["Fire", "Poison"],
+		genderRatio: {M: 0.875, F: 0.125},
+		baseStats: {hp: 45, atk: 49, def: 49, spa: 65, spd: 65, spe: 45},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+		heightm: 0.7,
+		weightkg: 6.9,
+		color: "Green",
+		evos: ["Ivysaur"],
+		eggGroups: ["Monster", "Grass"],
+	},
 };
